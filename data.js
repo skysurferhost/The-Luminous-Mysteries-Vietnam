37bd4a9e04ecb10ae95d620214911238450b8b74
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-07 00:23:02 -0500

    update

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "1. Đức Chúa Giêsu Chịu Phép Rửa Tại Sông Giođan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5299641627839335,
+          "pitch": 0.14053444062748355,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6114397633038564,
+          "pitch": -0.15833041838937767,
+          "title": "<p class=\"MsoNormal\">Đức Giê-su chịu phép rửa<o:p></o:p></p>",
+          "text": "Bấy giờ, Đức Giê-su từ miền Ga-li-lê đến sông Gio-đan, gặp ông Gio-an để xin ông làm phép rửa cho mình. Nhưng ông một mực can Người và nói: “Chính tôi mới cần được Ngài làm phép rửa, thế mà Ngài lại đến với tôi!” Nhưng Đức Giê-su trả lời: “Bây giờ cứ thế đã. Vì chúng ta nên làm như vậy để giữ trọn đức công chính.” Bấy giờ ông Gio-an mới chiều theo ý Người. Khi Đức Giê-su chịu phép rửa xong, vừa ở dưới nước lên, thì các tầng trời mở ra. Người thấy Thần Khí Thiên Chúa đáp xuống như chim bồ câu và ngự trên Người. Và có tiếng từ trời phán: “Đây là Con yêu dấu của Ta, Ta hài lòng về Người.”<div><p class=\"MsoNormal\">(Mátthêu 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "2. Đức Chúa Giêsu Dự Tiệc Cưới Cana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17689997262839974,
+          "pitch": -0.026220061447965293,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3410113397386212,
+          "pitch": 0.11641523917440821,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.18549050094204134,
+          "pitch": -0.23763109443604336,
+          "title": "Tiệc cưới Ca-na",
+          "text": "<div>Ngày thứ ba, có tiệc cưới tại Ca-na miền Ga-li-lê. Trong tiệc cưới có thân mẫu Đức Giê-su. Đức Giê-su và các môn đệ cũng được mời tham dự. Khi thấy thiếu rượu, thân mẫu Đức Giê-su nói với Người: “Họ hết rượu rồi.” Đức Giê-su đáp: “Thưa bà, chuyện đó can gì đến bà và tôi? Giờ của tôi chưa đến.” Thân mẫu Người nói với gia nhân: “Người bảo gì, các anh cứ việc làm theo.”</div><div><br></div><div>Ở đó có đặt sáu chum đá dùng vào việc thanh tẩy theo thói tục người Do-thái, mỗi chum chứa được khoảng tám mươi hoặc một trăm hai mươi lít nước. Đức Giê-su bảo họ: “Các anh đổ đầy nước vào chum đi!” Và họ đổ đầy tới miệng. Rồi Người nói với họ: “Bây giờ các anh múc và đem cho ông quản tiệc.” Họ liền đem cho ông. Khi người quản tiệc nếm thử nước đã hóa thành rượu (mà không biết rượu từ đâu ra, còn gia nhân đã múc nước thì biết), ông mới gọi tân lang lại và nói: “Ai ai cũng thết rượu ngon trước, và khi khách đã ngà ngà mới đãi rượu xoàng hơn. Còn anh, anh lại giữ rượu ngon mãi cho đến bây giờ.” Đức Giê-su đã làm dấu lạ đầu tiên này tại Ca-na miền Ga-li-lê và bày tỏ vinh quang của Người. Các môn đệ đã tin vào Người. Sau đó, Người cùng với thân mẫu, anh em và các môn đệ xuống Ca-phác-na-um và ở lại đó ít ngày.</div><div>(Gioan 2:1-11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "3. Đức Chúa Giêsu Rao Giảng Nước Trời và Kêu Gọi Sám Hối",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.009579539986813046,
+          "pitch": -0.26692904411839713,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7247348520871917,
+          "pitch": 0.07055712409420778,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.014038566703753474,
+          "pitch": -0.4708236215814203,
+          "title": "Đức Giê-su khai mạc công việc rao giảng",
+          "text": "Sau khi ông Gio-an bị nộp, Đức Giê-su đến miền Ga-li-lê rao giảng Tin Mừng của Thiên Chúa. Người nói: “Thời kỳ đã mãn, và Triều Đại Thiên Chúa đã đến gần. Anh em hãy hối cải và tin vào Tin Mừng.”<div>(Mác-cô 1:14-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "4. Đức Chúa Giêsu Biến Hình Trên Núi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5879398985307418,
+          "pitch": 0.35031539479094675,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.5492935955129816,
+          "pitch": 0.032476124460600886,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.197189731075694,
+          "pitch": 0.08712360171548994,
+          "title": "Đức Giê-su hiển dung",
+          "text": "Sáu ngày sau, Đức Giê-su đem các ông Phê-rô, Gia-cô-bê và Gio-an là em ông Gia-cô-bê đi theo mình. Người đưa các ông đi riêng ra một chỗ, tới một ngọn núi cao. Rồi Người biến đổi hình dạng trước mặt các ông. Dung nhan Người chói lọi như mặt trời, và y phục Người trở nên trắng tinh như ánh sáng. Và bỗng các ông thấy ông Mô-sê và ông Ê-li-a hiện ra đàm đạo với Người. Bấy giờ ông Phê-rô thưa với Đức Giê-su rằng: “Lạy Ngài, chúng con ở đây, thật là hay! Nếu Ngài muốn, con xin dựng tại đây ba cái lều, một cho Ngài, một cho ông Mô-sê, và một cho ông Ê-li-a.” Ông còn đang nói, chợt có đám mây sáng ngời bao phủ các ông, và có tiếng từ đám mây phán rằng: “Đây là Con yêu dấu của Ta, Ta hài lòng về Người. Các ngươi hãy vâng nghe lời Người!” Nghe vậy, các môn đệ kinh hoàng, ngã sấp mặt xuống đất. Bấy giờ Đức Giê-su lại gần, chạm vào các ông và bảo: “Trỗi dậy đi, đừng sợ!” Các ông ngước mắt lên, không thấy ai nữa, chỉ còn một mình Đức Giê-su mà thôi.<div>(Mátthêu 17:1-8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "5. Đức Chúa Giêsu Lập Bí Tích Thánh Thể",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8911809066809484,
+          "pitch": 0.12989975817713528,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07802354591362004,
+          "pitch": -0.34811315586330593,
+          "title": "<span style=\"font-size:12.0pt;line-height:115%;\nfont-family:&quot;Aptos&quot;,sans-serif;mso-fareast-font-family:Aptos;mso-fareast-theme-font:\nminor-latin;mso-bidi-font-family:Aptos;mso-ansi-language:EN-US;mso-fareast-language:\nEN-US;mso-bidi-language:AR-SA\">Đ</span><span style=\"font-size:12.0pt;\nline-height:115%;font-family:&quot;Aptos&quot;,sans-serif;mso-ascii-theme-font:minor-latin;\nmso-fareast-font-family:Aptos;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">ức\nGiê-su lập bí tích Thánh Thể</span>",
+          "text": "Cũng trong bữa ăn, Đức Giê-su cầm lấy bánh, dâng lời chúc tụng, rồi bẻ ra, trao cho môn đệ và nói: “Anh em cầm lấy mà ăn, đây là mình Thầy.” Rồi Người cầm lấy chén, dâng lời tạ ơn, trao cho môn đệ và nói: “Tất cả anh em hãy uống chén này, vì đây là máu Thầy, máu Giao Ước, đổ ra cho muôn người được tha tội.<div>(Mátthêu 26:26-28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
